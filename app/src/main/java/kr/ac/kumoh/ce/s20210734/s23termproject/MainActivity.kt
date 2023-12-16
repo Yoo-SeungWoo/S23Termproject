328a8479e8feb6aa884311ba287658e6aadbe119
USER <yoosw0329@naver.com>
2023-12-17 01:16:17 +0900

    💄 AsyncImage 및 Card 사용

--- app/src/main/java/kr/ac/kumoh/ce/s20210734/s23termproject/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20210734/s23termproject/MainActivity.kt
@@ -4,25 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import kr.ac.kumoh.ce.s20210734.s23termproject.ui.theme.S23TermprojectTheme
 
 class MainActivity : ComponentActivity() {
@@ -64,23 +81,47 @@ fun SkinList(list: List<Skin>) {
 
 @Composable
 fun SkinItem(skin: Skin) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color(0xffffffcc))
-            .padding(16.dp)
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier.clickable { expanded = !expanded },
+        elevation = CardDefaults.cardElevation(8.dp)
     ) {
-        TextSkin("${skin.theme + skin.name}")
-        TextRole("이 챔피언의 역할은 ${skin.role} ${skin.subrole} 입니다")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .background(Color(255, 220, 220))
+                .padding(8.dp)
+        ) {
+            AsyncImage(
+                model = skin.skin_image,
+                contentDescription = "챔피언 스킨 이미지",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(percent = 10)),
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                TextSkin("${skin.theme} ${skin.name}")
+                TextRole("이 챔피언의 역할은 ${skin.role}와 ${skin.subrole} 입니다")
+            }
+        }
+        AnimatedVisibility(visible = expanded) {
+            skin.script?.let {Text(it)}
+        }
     }
 }
 
 @Composable
 fun TextSkin(name :String) {
-    Text(name, fontSize = 30.sp)
+    Text(name, fontSize = 25.sp)
 }
 
 @Composable
 fun TextRole(role: String) {
-    Text(role, fontSize = 20.sp)
+    Text(role, fontSize = 15.sp)
 }
