93954d70ae7a09dbea24f2e3300c27e02d402e1f
USER <yoosw0329@naver.com>
2023-12-17 02:11:49 +0900

    정리

--- app/src/main/java/kr/ac/kumoh/ce/s20210734/s23termproject/SkinScreen.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20210734/s23termproject/SkinScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
